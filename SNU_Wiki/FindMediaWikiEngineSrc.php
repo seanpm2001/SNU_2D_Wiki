f6ce8b2e51fc34f317170b0ef2d72436b04f9d57
Sean P. Myrick V19.0.2 <65933340+seanpm2001@users.noreply.github.com>
2020-06-29 13:40:36 -0700

    4 root files
    The 4 root files for this project, in HTML and PHP formats.

---
+++ SNU_Wiki/FindMediaWikiEngineSrc.php
@@ -0,0 +1,17 @@
+<?// Script that calls the MediaWiki engine
+$startOutTalk = str("Finding the MediaWiki engine");
+print("$startOutTalk");
+""""""//
+/* Scripting not available */
+'''''' // script goes here //
+'''''' // script goes here //
+/* Scripting not available */
+""""""//
+// Not found
+/* File info:
+File Type: *.php file (PHP Hypertext Preprocessor file)
+Compatibility: PHP 7
+First version: 1 (October 11th 2019)
+Current version: 1 (October 11th 2019)
+Latest version: 1 (October 11th 2019)
+*/?>
